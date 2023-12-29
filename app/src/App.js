53cff68bafb74c2703915d60e7f8e38da3fc242d
Obua Stella <stellaobua123@gmail.com>
2023-12-29 18:51:08 +0100

    Created the React app and added tailwind

---
+++ app/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-rose-950 text-center text-5xl">
+        Swift Asset Manager (Sam)
+      </h1>
+    </div>
+  );
+}
+
+export default App;
